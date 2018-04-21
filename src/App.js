51c43245d74a59f932726f15fa24362f544b283b
Dylan Ellison <dylancellison@gmail.com>
2018-04-20 23:20:50 -0500

    logged BTC price to dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import PriceList from './components/price-list';
 
+const url = "https://api.gdax.com/products/BTC-USD/ticker";
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentBtcPrice: 8000,
+      currentLtcPrice: null,
+      currentEthPrice: null
+    }
+
+    this.firstRequest(url);
+  };
+
+  firstRequest(url) {
+      fetch(url).then((response) => {
+          return response.json();
+      }).then((myJson) => {
+          let price = Number(myJson.price);
+          price = parseFloat(price);
+          this.setState({currentBtcPrice: price});
+          console.log(price);
+
+      }).catch(function(err) {
+          console.log("the Gdax API call did not go through!! try again")
+      })
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
@@ -15,6 +46,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <PriceList />
+        {this.state.currentBtcPrice}
       </div>
     );
   }
