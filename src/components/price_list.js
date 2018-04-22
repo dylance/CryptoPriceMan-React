8623c1fdb78524919e835e8bda1625057ce1b720
Dylan Ellison <dylancellison@gmail.com>
2018-04-21 23:08:40 -0500

    added border on hover for logos

--- src/components/price_list.js
+++ src/components/price_list.js
@@ -4,16 +4,22 @@ const PriceList = (props) => {
   return (
     <div>
     <div className="Price-container col-sm-4">
-    <img src={process.env.PUBLIC_URL + '/img/btc.png'} height="35" alt="Bitcoin"/>
+    <span>
+    <img id="BTC-Logo" src={process.env.PUBLIC_URL + '/img/btc.png'} height="35" alt="Bitcoin"/>
       {props.btcPrice}
+    </span>
     </div>
     <div className="Price-container col-sm-4">
-    <img src={process.env.PUBLIC_URL + '/img/ltc.png'} height="35" alt="Bitcoin"/>
+    <span>
+    <img id="LTC-Logo" src={process.env.PUBLIC_URL + '/img/ltc.png'} height="35" alt="Bitcoin"/>
       {props.ltcPrice}
+    </span>
     </div>
     <div className="Price-container col-sm-4">
-    <img src={process.env.PUBLIC_URL + '/img/eth.png'} height="35" alt="Bitcoin"/>
+    <span>
+    <img id="ETH-Logo" src={process.env.PUBLIC_URL + '/img/eth.png'} height="35" alt="Bitcoin"/>
       {props.ethPrice}
+    </span>
     </div>
     </div>
   );
