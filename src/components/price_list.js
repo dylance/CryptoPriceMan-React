a2c64b5c3b26d34557721a7a3071e2406656497f
Dylan Ellison <dylancellison@gmail.com>
2018-04-21 22:56:57 -0500

    added styling to price_list

--- src/components/price_list.js
+++ src/components/price_list.js
@@ -3,15 +3,15 @@ import React from 'react';
 const PriceList = (props) => {
   return (
     <div>
-    <div className="col-sm-4">
+    <div className="Price-container col-sm-4">
     <img src={process.env.PUBLIC_URL + '/img/btc.png'} height="35" alt="Bitcoin"/>
       {props.btcPrice}
     </div>
-    <div className="col-sm-4">
+    <div className="Price-container col-sm-4">
     <img src={process.env.PUBLIC_URL + '/img/ltc.png'} height="35" alt="Bitcoin"/>
       {props.ltcPrice}
     </div>
-    <div className="col-sm-4">
+    <div className="Price-container col-sm-4">
     <img src={process.env.PUBLIC_URL + '/img/eth.png'} height="35" alt="Bitcoin"/>
       {props.ethPrice}
     </div>
