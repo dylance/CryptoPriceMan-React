91fdb24469e11895890063a2656838739543c6ea
Dylan Ellison <dylancellison@gmail.com>
2018-04-26 17:01:01 -0500

    made websocket connection

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+// npm install coinbase/gdax-node
+const Gdax = require('gdax');
+const websocket = new Gdax.WebsocketClient(['BTC-USD']);
+const cors = require('cors')
+app.use(cors())
+
 const port = 45566;
 
 app.use(require('compression')());
@@ -8,5 +14,21 @@ app.use(require('body-parser').urlencoded({ extended:true }));
 
 app.use(express.static('build'));
 
+var priceOfBtc = '';
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+websocket.on('message', data => {
+ priceOfBtc = data.price;
+});
+websocket.on('error', err => {
+ /* handle error */
+});
+websocket.on('close', () => {
+ /* ... */
+});
+
+app.get('/btcprice', (req, res) => {
+ res.send(priceOfBtc)
+})
+
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, console.log('data on 45566/btcprice'))
