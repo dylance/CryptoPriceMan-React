670024cd29784a57d2e8adafce4966c2896a43f6
Dylan Ellison <dylancellison@gmail.com>
2018-04-30 23:24:22 -0500

    only socked price btc logo spins

--- src/components/instant_btc_price.js
+++ src/components/instant_btc_price.js
@@ -6,7 +6,7 @@ const InstantBtcPrice = (props) => {
     <div>
       <div className="Price-container col-sm-12">
         <span id="BTCSpan">
-          <img id="BTC-Logo" className="logo" src={process.env.PUBLIC_URL + '/img/btc.png'} height="70" alt="Bitcoin"/>
+          <img id="BTC-Socket-Logo" className="logo" src={process.env.PUBLIC_URL + '/img/btc.png'} height="70" alt="Bitcoin"/>
           <span id="BTC-Price">{Number(props.btcSocketPrice).toFixed(2)}</span>
           <span></span>
         </span>
