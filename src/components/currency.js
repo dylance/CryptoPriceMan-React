c3c643c9521ddb75ee4ed74ecd739ee993fc7259
Dylan Ellison <dylancellison@gmail.com>
2018-05-02 00:37:27 -0500

    changed size of currency icons

--- src/components/currency.js
+++ src/components/currency.js
@@ -11,7 +11,7 @@ const Currency = (props) => {
             </div>
             <div className="col-xs-3">
                 <img
-                  src={process.env.PUBLIC_URL + '/img/usd.png'} height="90" alt="USD"
+                  src={process.env.PUBLIC_URL + '/img/usd.png'} height="70" alt="USD"
                   onClick={
                      () => props.onUsdClick(props.currency)
                   }
@@ -20,7 +20,7 @@ const Currency = (props) => {
             </div>
             <div className="col-xs-3">
               <img
-                src={process.env.PUBLIC_URL + '/img/eur.png'} height="90" alt="USD"
+                src={process.env.PUBLIC_URL + '/img/eur.png'} height="70" alt="USD"
                 onClick={
                    () => props.onEurClick(props.currency)
                 }
