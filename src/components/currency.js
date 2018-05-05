fd2574e9f15ac4087bdfe7d7eb5375c8f712af1b
Dylan Ellison <dylancellison@gmail.com>
2018-05-04 21:14:23 -0500

    feat: currenies get larger on hover

--- src/components/currency.js
+++ src/components/currency.js
@@ -10,7 +10,7 @@ const Currency = (props) => {
                 <span>Select Currency:</span>
             </div>
             <div className="col-xs-3">
-                <img
+                <  img className="currency-logo"
                   src={process.env.PUBLIC_URL + '/img/usd.png'} height="70" alt="USD"
                   onClick={
                      () => props.onUsdClick(props.currency)
@@ -19,7 +19,7 @@ const Currency = (props) => {
                 />
             </div>
             <div className="col-xs-3">
-              <img
+              <img className="currency-logo"
                 src={process.env.PUBLIC_URL + '/img/eur.png'} height="70" alt="USD"
                 onClick={
                    () => props.onEurClick(props.currency)
