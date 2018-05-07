8c4c700992065365b903f6db8f6fa235764c8b4b
Dylan Ellison <dylancellison@gmail.com>
2018-05-06 22:49:49 -0500

    style: created stylesheet for instant_btc_price component

--- src/components/instant_btc_price.js
+++ src/components/instant_btc_price.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
+import classes from './instant_btc_price.css'
+
 const InstantBtcPrice = (props) => {
 
   return (
     <div>
       <div className="Price-container col-sm-12">
-        <span id="BTCSpan">
+        <span>
           <img id="BTC-Socket-Logo" className="logo" src={process.env.PUBLIC_URL + '/img/btc.png'} height="70" alt="Bitcoin"/>
           <span id="BTC-Price">{Number(props.btcSocketPrice).toFixed(2)}</span>
           <span></span>
         </span>
       </div>
       <div className="Price-container col-sm-12">
-        <span id="BTCSpan"height="75">
+        <span height="75">
         </span>
       </div>
     </div>
